eab5eef944b77d5f66d0c40dd81af95c8cb4ad8f
Clay Rosenthal <git@mail.clayrosenthal.me>
2025-01-16 01:02:26 -0800

    Removing update-gen, just having gen copy files

--- build.rs
+++ build.rs
@@ -67,22 +67,20 @@ fn generate_protobufs() -> std::io::Result<()> {
 
     config.compile_protos(&protos, &[protobufs_dir]).unwrap();
 
-    #[cfg(feature = "update-gen")]
-    {
-        let out_dir = std::env::var("OUT_DIR").unwrap();
-        let gen_dir = "src/generated-protobufs/";
-        walkdir::WalkDir::new(out_dir)
-            .into_iter()
-            .map(|e| e.unwrap())
-            .filter(|e| e.path().extension().is_some() && e.path().extension().unwrap() == "rs")
-            .for_each(|e| {
-                let file_name = e.path().file_name().unwrap();
-                let _ = std::fs::copy(
-                    e.path(),
-                    std::path::Path::new(gen_dir).join(file_name),
-                );
-            });
-    }
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let gen_dir = "src/generated-protobufs/";
+    walkdir::WalkDir::new(out_dir)
+        .into_iter()
+        .map(|e| e.unwrap())
+        .filter(|e| e.path().extension().is_some() && e.path().extension().unwrap() == "rs")
+        .for_each(|e| {
+            let file_name = e.path().file_name().unwrap();
+            let _ = std::fs::copy(
+                e.path(),
+                std::path::Path::new(gen_dir).join(file_name),
+            );
+        });
 
     Ok(())
 }
